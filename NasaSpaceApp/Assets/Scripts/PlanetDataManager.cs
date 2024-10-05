a5a99367a66aea6709f828ea088dedee1352d165
Alexis CG <113210633+AlexisCottierGuilly@users.noreply.github.com>
2024-10-05 12:22:12 -0400

    Add gameobject to scene

--- NasaSpaceApp/Assets/Scripts/PlanetDataManager.cs
+++ NasaSpaceApp/Assets/Scripts/PlanetDataManager.cs
@@ -6,13 +6,11 @@ public class PlanetDataManager : MonoBehaviour
 {
     public List<PlanetarySystem> planetarySystems = new List<PlanetarySystem>();
     
-    // Start is called before the first frame update
     void Start()
     {
-        Debug.Log($"First planet of first system: {planetarySystems[0].planets[0].name}");
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
         
