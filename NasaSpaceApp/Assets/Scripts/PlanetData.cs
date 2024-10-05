31f64db24e93ad06ac2397cdacb4384467fd89af
Melkor <melkor5456@gmail.com>
2024-10-05 13:57:36 -0400

    jalcule de la position des planetes

--- NasaSpaceApp/Assets/Scripts/PlanetData.cs
+++ NasaSpaceApp/Assets/Scripts/PlanetData.cs
@@ -5,27 +5,30 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlanetData", menuName = "Space/PlanetData", order = 2)]
 public class PlanetData : ScriptableObject
 {
-<<<<<<< Updated upstream
     public double semiMajor;  // in km
     public double eccentricity;  // range 0-1
     public double mass;  // in 1x10e24 kg
+    public double period; // in s
 
     [Space]
-    public double initialTime;  // in s
+    public double initialTime;  // in s, time since perihelion
 
-    [Space]
-    public double time;  // in s
-    public double radius;  // in km
-=======
-    public double semiMajor;
-    public double eccentricity;
-    public double mass;
-    public double period;
-    public double initialTime;
-    public double radius;
-    public double angle;
-    void CalculatePosition(double time) {
-        double realTime = time + initialTime;
+
+    public Vector2 CalculatePosition(double time) {
+        double realTime = initialTime + time;
+        double meanAnomaly = 2 * Mathf.PI * realTime / period;
+        //calculate eccentric anomaly
+        double eccentricAnomaly = meanAnomaly;
+        double delta = 1;
+        while (delta > 0.0001) {
+            double nextEccentricAnomaly = meanAnomaly + eccentricity * Mathf.Sin(eccentricAnomaly);
+            delta = Mathf.Abs(nextEccentricAnomaly - eccentricAnomaly);
+            eccentricAnomaly = nextEccentricAnomaly;
+        }
+        //calculate true anomaly
+        double trueAnomaly = 2 * Mathf.Atan(Mathf.Sqrt((1 + eccentricity) / (1 - eccentricity)) * Mathf.Tan(eccentricAnomaly / 2));
+        //calculate distance
+        double distance = semiMajor * (1 - eccentricity * Mathf.Cos(eccentricAnomaly));
+        return Vector2(distance, trueAnomaly);
     }
->>>>>>> Stashed changes
 }
