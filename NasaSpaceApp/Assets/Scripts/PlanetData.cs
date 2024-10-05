2815432e5c862ed4be02b09ae9ae867f2eaf29ea
Melkor <melkor5456@gmail.com>
2024-10-05 13:49:26 -0400

    je rajoute les trajectoires de base

--- NasaSpaceApp/Assets/Scripts/PlanetData.cs
+++ NasaSpaceApp/Assets/Scripts/PlanetData.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlanetData", menuName = "Space/PlanetData", order = 2)]
 public class PlanetData : ScriptableObject
 {
+<<<<<<< Updated upstream
     public double semiMajor;  // in km
     public double eccentricity;  // range 0-1
     public double mass;  // in 1x10e24 kg
@@ -15,4 +16,16 @@ public class PlanetData : ScriptableObject
     [Space]
     public double time;  // in s
     public double radius;  // in km
+=======
+    public double semiMajor;
+    public double eccentricity;
+    public double mass;
+    public double period;
+    public double initialTime;
+    public double radius;
+    public double angle;
+    void CalculatePosition(double time) {
+        double realTime = time + initialTime;
+    }
+>>>>>>> Stashed changes
 }
