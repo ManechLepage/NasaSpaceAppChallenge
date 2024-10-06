658568033cc8d50154a3989df3e2e4de571cb884
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2024-10-06 13:09:59 -0400

    Muer

---
+++ NasaSpaceApp/Assets/Scripts/LoreManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoreManager : MonoBehaviour
+{
+    public List<string> TutorialTexts;
+    public List<string> level1Texts;
+    public List<string> level2Texts;
+    public List<string> level3Texts;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+    }
+}
