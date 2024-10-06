7a1cdd814f9e741e3334c1afbf972af71d64c10a
Melkor <melkor5456@gmail.com>
2024-10-06 13:03:22 -0400

    eh oui jamie

--- NasaSpaceApp/Assets/Scripts/TrajectoryRenderer.cs
+++ NasaSpaceApp/Assets/Scripts/TrajectoryRenderer.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class TrajectoryRenderer : MonoBehaviour
 {   
     public GameObject trajectoryPrefab;
-    private List<GameObject> renderedTrajectory = new List<GameObject>();
+    private List<List<GameObject>> renderedTrajectory = new List<List<GameObject>>();
     // Update is called once per frame
+    public void newLaunch() {
+        renderedTrajectory.Add(new List<GameObject>());
+    }
     public void addTrajectoryPoint(Vector2 point)
     {
         GameObject newPoint = Instantiate(trajectoryPrefab, transform);
         newPoint.transform.localPosition = new Vector3((float)point.x, (float)point.y + 0.25f, 0);
         newPoint.transform.localScale = new Vector3(0.75f, 0.75f, 1f);
-        renderedTrajectory.Add(newPoint); 
+        renderedTrajectory[renderedTrajectory.Count - 1].Add(newPoint); 
     }
-    public void deleteTrajectory()
+    public void fadeTrajectories()
     {
-        foreach (GameObject point in renderedTrajectory) {
-            Destroy(point);
+        for (int i = 0; i < renderedTrajectory.Count; i++)
+        {
+            foreach (GameObject point in renderedTrajectory[i])
+            {
+                Color color = point.GetComponent<SpriteRenderer>().color;
+                color.a = ((float)(i+1))/(renderedTrajectory.Count+1);
+                point.GetComponent<SpriteRenderer>().color = color;
+            }
         }
-        renderedTrajectory.Clear();
     }
 }
