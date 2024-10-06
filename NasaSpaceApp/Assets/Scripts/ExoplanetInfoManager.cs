b6bd2cc357c5bb3d9af1c02b89dde01e8d96de6f
Alexis CG <113210633+AlexisCottierGuilly@users.noreply.github.com>
2024-10-06 13:23:36 -0400

    Aaaaaaaaaa

--- NasaSpaceApp/Assets/Scripts/ExoplanetInfoManager.cs
+++ NasaSpaceApp/Assets/Scripts/ExoplanetInfoManager.cs
@@ -16,6 +16,7 @@ public class ExoplanetInfoManager : MonoBehaviour
     public TextMeshProUGUI planetGravity;
     public TextMeshProUGUI planetDistance;
     public TextMeshProUGUI planetPeriod;
+    public GameObject triangle;
 
     void Start()
     {
@@ -72,13 +73,42 @@ public class ExoplanetInfoManager : MonoBehaviour
         this.planetData = planetData;
         planetName.text = planetData.name;
         planetType.text = planetData.type.ToString();
-        planetMass.text = planetData.mass.ToString() + " Earth Masses";
+        planetMass.text = planetData.mass.ToString() + " x Earth";
         planetDistance.text = planetData.semiMajor.ToString() + " AU";
-        planetPeriod.text = planetData.period.ToString() + " Earth Days";
+        planetPeriod.text = (Mathf.Round(planetData.period / 31557600f * 1000f) / 1000f).ToString() + " Years";
 
         //preview.sprite = planetData.planetSprite;
         //float gravity = 6.67f * 10f**-11f * planetData.mass / (planetData.semiMajor * 1.496f**8f)**2f;
-        //planetGravity.text = 6.67f*10f**-11f *  + " m/s^2";
+        //planetGravity.text = 6.67f*10f**-11f *  + " m/s²";
+
+        Vector2 screenToWorld = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        Rect panelRect = planetInfoPanel.GetComponent<RectTransform>().rect;
+        Vector2 panelSize = new Vector2(-panelRect.width, -panelRect.height);
+        Vector2 panelWorldSize = Camera.main.ScreenToWorldPoint(panelSize);
+        panelWorldSize = new Vector2(Mathf.Abs(panelWorldSize.x), Mathf.Abs(panelWorldSize.y));
+
+        bool right = position.x + panelWorldSize.x < screenToWorld.x * 1.67857142857f;
+        if (right)
+            planetInfoPanel.transform.position = new Vector2(position.x + panelWorldSize.x / 4, position.y);
+        else
+            planetInfoPanel.transform.position = new Vector2(position.x - panelWorldSize.x / 4, position.y);
+        
+        planetInfoPanel.transform.position = new Vector2(planetInfoPanel.transform.position.x, planetInfoPanel.transform.position.y);
+        Vector2 panelScreenPosition = Camera.main.WorldToScreenPoint(planetInfoPanel.transform.position);
+        planetInfoPanel.transform.position = panelScreenPosition;
+
+        if (!right)
+        {
+            float rotationToGetTo180 = 180 - triangle.transform.rotation.eulerAngles.y;
+            triangle.transform.Rotate(new Vector3(0, rotationToGetTo180, 0));
+            triangle.transform.position = new Vector2(panelScreenPosition.x + panelRect.width / 1.5f + 6.7f, panelScreenPosition.y);
+        }
+        else
+        {
+            float rotationToGetTo0 = 0 - triangle.transform.rotation.eulerAngles.y;
+            triangle.transform.Rotate(new Vector3(0, rotationToGetTo0, 0));
+            triangle.transform.position = new Vector2(panelScreenPosition.x - panelRect.width / 2f - 6.7f, panelScreenPosition.y);
+        }
     }
 
     public void ShowPlanetInfo()
