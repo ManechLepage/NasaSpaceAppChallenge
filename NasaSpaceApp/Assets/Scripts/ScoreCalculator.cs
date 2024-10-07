16bba10401eb8cc5f8f666ad3ae778faefa479bd
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2024-10-06 21:22:41 -0400

    Mousson!!!

--- NasaSpaceApp/Assets/Scripts/ScoreCalculator.cs
+++ NasaSpaceApp/Assets/Scripts/ScoreCalculator.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class ScoreCalculator : MonoBehaviour
 {
     public PlanetDataManager planetDataManager;
+    public GameObject soundEffect;
     
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -15,6 +16,7 @@ public class ScoreCalculator : MonoBehaviour
             //other.gameObject.SetActive(false);
             planetDataManager.DidScore(CalculateScore(other.gameObject.transform.position.y));
             other.gameObject.GetComponent<Trajectory>().Reset();
+            soundEffect.GetComponent<AudioSource>().Play();
         }
     }
 
